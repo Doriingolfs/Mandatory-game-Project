31c291d35e16f06ba0d1448215a37883caa864a3
Doriingolfs <hall0346@easv365.dk>
2019-09-20 11:22:37 +0200

    renaming

--- TTT.php
+++ TTT.php
@@ -7,17 +7,14 @@
     <title>Tick Tack Toe</title>
 </head>
 <body>
-    <form method="POST" action="index.php">
+    <form method="POST" action="index2.php">
         <?php
             $error = false; $x_wins = false; $o_wins = false; $count = 0;
             for ($id = 1; $id <= 9; $id++) 
             {   
             
-                if ($id == 4 or $id == 7) print "<br>";
-                {
-                    print "<input name = $id type = text size =8;";
-                }
-            
+                if ($id == 4 or $id == 7) print "<br>";                
+                print "<input name =$id type=text size=8";                            
                 
                 if (isset($_POST['submit']) and !empty($_POST[$id]))
                 {
@@ -28,8 +25,7 @@
                         
                         
                         for ($a = 1, $b = 2, $c = 3; $a<= 7, $b <= 8, $c <=9; $a+=3, $b+=3, $c+=3)
-                        {
-                            
+                        {      /*                      
                             if ($_POST["$a"] == $_POST["$b"] and $_POST["$b"] == $_POST["$c"]) 
                             {
                                 if ($_POST["$a"] == "x") 
@@ -41,12 +37,13 @@
                                     $o_wins = true;
                                 }
                             }
+                            */
                             
                         }
                         
 
-                        for ($a = 1, $b = 4, $c = 7; $a<= 3, $b <= 6, $c <=9; $a+=3, $b+=3, $c+=3)
-                        {
+                        for ($a = 1, $b = 4, $c = 7; $a<= 3, $b <= 6, $c <=9; $a+=1, $b+=1, $c+=1)
+                        {/*
                             if ($_POST["$a"] == $_POST["$b"] and $_POST["$b"] == $_POST["$c"]) 
                             {
                                 if ($_POST["$a"] == "x") 
@@ -58,10 +55,11 @@
                                     $o_wins = true;
                                 }
                             }
+                            */
                         }
 
                         for ($a = 1, $b = 5, $c = 9; $a<= 3, $b <= 5, $c <=7; $a+=2, $b+=0, $c-=2)
-                        {
+                        {/*
                             if ($_POST["$a"] == $_POST["$b"] and $_POST["$b"] == $_POST["$c"]) 
                             {
                                 if ($_POST["$a"] == "x") 
@@ -72,8 +70,10 @@
                                 {
                                     $o_wins = true;
                                 }                       
-                            }                        
+                            }   
+                            */                     
                         }
+                        
                         
                     }
                 }                
